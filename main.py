651fb95b7c2844b8611ded5ea2dd74f4212184c7
Emanuele Fia <emanuele.fia@gmail.com>
2017-05-23 19:50:31 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+Created on Oct 1, 2014
+@author: fabiomignini
+@author: gabrielecastellano
+"""
+
+import falcon
+import logging
+import os
+import inspect
+
+from threading import Thread
+from ServiceOrchestrator.config import Configuration
+
+
+conf = Configuration()
+
+# set log level
+if conf.DEBUG is True:
+    log_level = logging.DEBUG
+    requests_log = logging.getLogger("requests")
+    requests_log.setLevel(logging.WARNING)
+elif conf.VERBOSE is True:
+    log_level = logging.INFO
+    requests_log = logging.getLogger("requests")
+    requests_log.setLevel(logging.WARNING)
+else:
+    log_level = logging.WARNING
+
+# format = '%(asctime)s %(filename)s %(funcName)s %(levelname)s %(message)s'
+log_format = '%(asctime)s %(levelname)s %(message)s - %(filename)s:%(lineno)s'
+
+logging.basicConfig(filename=conf.LOG_FILE, level=log_level, format=log_format, datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.debug("Service Orchestrator Starting")
+print("Welcome to the User-oriented Service Orchestrator Application")
+logging.info("Starting Service Orchestrator application")
+
+# start the dd client to receive information about domains
+base_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+dd_client = DDClient(conf.DD_NAME, conf.BROKER_ADDRESS, conf.DD_CUSTOMER, conf.DD_KEYFILE)
+thread = Thread(target=dd_client.start)
+thread.start()
+
+logging.info("DoubleDecker Client Successfully started")
